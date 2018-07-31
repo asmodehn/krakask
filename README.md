00b1ba5f8b5dac52ff82ce1d4372f868ebf98b36
AlexV <asmodehn@gmail.com>
2018-07-31 18:54:02 +0200

    first code dump. only servertime made async as example.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # krakask
 Asynchronous Kraken API
+
+
+This work spawned out of : 
+- https://github.com/veox/python3-krakenex
+- https://github.com/dominiktraxl/pykrakenapi
+
+and the need for asynchronous HTTP requests. 
---
+++ krakask/_version.py
@@ -0,0 +1,9 @@
+""" `krakask` package metadata.
+
+This module is used to store variables that need to be single-sourced
+throughout the project.
+
+"""
+
+__version__ = '0.1.1'
+__url__ = 'https://github.com/asmodehn/krakask'
